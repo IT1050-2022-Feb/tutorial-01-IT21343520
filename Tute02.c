1cff36e3c3008af5e731d0ffabfe67e7dd3c8976
Rovinya Wijerama <99579169+IT21343520@users.noreply.github.com>
2022-02-13 18:31:22 +0530

    Update Tute02.c

--- Tute02.c
+++ Tute02.c
@@ -16,9 +16,24 @@ Amount = 20 x 50 = 1000
 Distance -> 50
 Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
-#include <stdio.h>
-
-int main() {
-  
-  return 0;
+#include<stdio.h>
+int main()
+{
+	int x;//distance
+	float amount;
+	printf("input the distance the vehicle has travelled :");
+	scanf("%d",&x);
+	
+	if(x>=30)
+	{
+		amount=(30*50)+(x-30)*40;
+	}
+	else
+	{
+		amount=x*50;
+	}
+	
+	printf("paid for a rented vehicle :%.2f",amount);
+	
+	return 0;
 }
