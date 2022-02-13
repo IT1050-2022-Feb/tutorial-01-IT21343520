526f75fe17f6f2d7cd3d60c417d11412e55e685f
Rovinya Wijerama <99579169+IT21343520@users.noreply.github.com>
2022-02-13 18:36:36 +0530

    Update Tute03.c

--- Tute03.c
+++ Tute03.c
@@ -11,9 +11,25 @@ sum = 1+2+3+....+ 99+100 = 5050
 n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
-#include <stdio.h>
-int main() {
-  
-  return 0;
+#include<stdio.h>
+int main()
+{
+	int x;
+	int n=1;
+	int tot=0;
+	
+	printf("enter number :");
+	scanf("%d",&x);
+	
+	while(n<=x)
+	{
+		
+		tot=tot+n;
+		n=n+1;
+		
+	}
+	printf("sum is :%d",tot);
+	
+	
+	return 0;
 }
-
